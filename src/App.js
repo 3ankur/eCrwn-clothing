02ef9066efc88715f9a00fce31bc94bcdf684e32
Ankur Vishwakarma <avishwakarma@anant.saama.com>
2019-09-29 16:03:17 +0530

    create app using create-react-app and added homepage code changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './homepage.styles.scss';
+import HomePage  from "./homepage.component";
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
